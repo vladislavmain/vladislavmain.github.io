de62a6e38290ac1288cbcbe7111568ccb8238fbd
vladislavmain <vladislavmainemail@yandex.ru>
2018-05-18 16:25:25 +0300

    remove *

--- my.js
+++
@@ -1,8 +0,0 @@
-
-  $(function(){
-  $('.slider').slick({
-      appendArrows: $('.arrow'),
-            prevArrow: '<button id="prev" type="button" class="btn btn-prev"><i class="fa  fa-chevron-left" aria-hidden="true"></i></button>',
-            nextArrow: '<button id="next" type="button" class="btn btn-next"><i class="fa  fa-chevron-right" aria-hidden="true"></i></button>'
-  });
-});

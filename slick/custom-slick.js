8e8c197846f3997926918afda0a291de94382638
vladislavmain <vladislavmainemail@yandex.ru>
2018-07-26 18:55:12 +0300

    ffwefw

---
+++ slick/custom-slick.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.home-slider').slick({
+  	 dots: true,
+    slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3500,
+  fade: true,
+  arrows: false,
+  cssEase: 'linear'
+  });
+});
